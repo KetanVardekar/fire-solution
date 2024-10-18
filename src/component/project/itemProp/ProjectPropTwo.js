27462c212467447ac435115df0737aefba536d4b
Ketan Vardekar <ketan@elegantmicroweb.com>
2024-10-18 22:41:17 +0530

    Changed the image of director

--- src/component/project/itemProp/ProjectPropTwo.js
+++ src/component/project/itemProp/ProjectPropTwo.js
@@ -3,40 +3,41 @@ import { Link } from 'react-router-dom';
 import { slugify } from '../../../utils';
 
 const PropOne = ({ projectStyle, portfolio }) => {
-  return (
-    <>
-      <div className={`project-grid ${projectStyle}`}>
-        <div
-          className="thumbnail"
-          style={{
-            width: '300px', 
-            height: '440px', 
-            overflow: 'hidden',
-            borderRadius: '15px',
-          }}
-        >
-          <img
-            src={process.env.PUBLIC_URL + portfolio.image}
-            alt="icon"
-            style={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-              objectPosition: 'center',
-            }}
-          />
-        </div>
-        <div className="content">
-          <span className="subtitle" style={{fontSize:'16px'}} >
-            {portfolio.category.map((cat, i) => (
-              <span key={i}>{cat}</span>
-            ))}
-          </span>
-          <h3 className="title" style={{fontSize:'30px'}}>{portfolio.title}</h3>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={`project-grid ${projectStyle}`}>
+                <div className="thumbnail">
+                    <img
+                        src={process.env.PUBLIC_URL + portfolio.image}
+                        alt="icon"
+                    />
+                </div>
+
+                <h3
+                    className="text-center"
+                    style={{
+                        marginTop: '10px', // Adjust this value as needed
+                        marginBottom: '5px', // Adjust this value as needed
+                        lineHeight: '1.4', // Optional: Adjust the line height
+                    }}
+                >
+                    {portfolio.category.map((cat, i) => (
+                        <span key={i}>{cat}</span>
+                    ))}
+                </h3>
+
+                <h6
+                    className="text-center"
+                    style={{
+                        marginTop: '5px', // Adjust this value as needed
+                        marginBottom: '15px', // Adjust this value as needed
+                    }}
+                >
+                    {portfolio.title}
+                </h6>
+            </div>
+        </>
+    );
 };
 
 export default PropOne;
