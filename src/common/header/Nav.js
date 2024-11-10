61e91396ca0e81f27c018b56ea846e1d0fba60e5
Ketan Vardekar <ketan@elegantmicroweb.com>
2024-11-10 22:14:35 +0530

    Updated images

--- src/common/header/Nav.js
+++ src/common/header/Nav.js
@@ -8,15 +8,15 @@ const Nav = () => {
         <nav className="mainmenu-nav">
             <ul className="mainmenu">
                 <li className="menu-item-has-children">
-                    <Link to={process.env.PUBLIC_URL + "/corporate-agency"}>HOME </Link>
+                    <Link to={process.env.PUBLIC_URL + "/corporate-agency"} style={{color:'#FF4500'}}>HOME </Link>
 
                 </li>
                 <li className="menu-item-has-children">
-                    <Link to={process.env.PUBLIC_URL + "/about-us"}>ABOUT US</Link>
+                    <Link to={process.env.PUBLIC_URL + "/about-us"} style={{color:'#FF4500'}}>ABOUT US</Link>
 
                 </li>
                 <li className="menu-item-has-children">
-                    <Link to="#">SERVICES <FaAngleDown /></Link>
+                    <Link to="#" style={{color:'#FF4500'}}>SERVICES <FaAngleDown /></Link>
                     <ul className="axil-submenu">
                         <li><Link to={process.env.PUBLIC_URL + "/fire-hydrant-system"}>FIRE HYDRANT SYSTEM</Link></li>
                         <li><Link to={process.env.PUBLIC_URL + "/fire-sprinkler-system"}>FIRE SPRINKLER SYSTEM</Link></li>
@@ -28,19 +28,19 @@ const Nav = () => {
                 </li>
 
                 <li className="menu-item-has-children">
-                    <Link to={process.env.PUBLIC_URL + "/products"}>PRODUCTS</Link>
+                    <Link to={process.env.PUBLIC_URL + "/products"} style={{color:'#FF4500'}}>PRODUCTS</Link>
 
                 </li>
 
                 <li className="menu-item-has-children">
-                    <a href={process.env.PUBLIC_URL + "/images/others/Catalogue.pdf"} target="_blank" rel="noopener noreferrer">
+                    <a href={process.env.PUBLIC_URL + "/images/others/Catalogue.pdf"} target="_blank" rel="noopener noreferrer" style={{color:'#FF4500'}}>
                         BROCHURES
                     </a>
                 </li>
 
 
                 <li className="menu-item-has-children">
-                    <Link to={process.env.PUBLIC_URL + "/contact"}>CONTACT US</Link>
+                    <Link to={process.env.PUBLIC_URL + "/contact"} style={{color:'#FF4500'}}>CONTACT US</Link>
 
                 </li>
             </ul>
--- src/component/project/itemProp/ProjectPropOne.js
+++ src/component/project/itemProp/ProjectPropOne.js
@@ -3,29 +3,29 @@ import { Link } from 'react-router-dom';
 import { slugify } from '../../../utils';
 
 
-const PropOne = ({projectStyle, portfolio}) => {
-	
-    return (
+const PropOne = ({ projectStyle, portfolio }) => {
+
+	return (
 		<>
 			<div className={`project-grid ${projectStyle}`}>
 				<div className="thumbnail">
-				<Link to={process.env.PUBLIC_URL + `/project-details/${slugify(portfolio.title)}`}>
-					<img src={process.env.PUBLIC_URL + portfolio.image} alt="icon" />
-				</Link>
+					<Link to={process.env.PUBLIC_URL + `/project-details/${slugify(portfolio.title)}`}>
+						<img src={process.env.PUBLIC_URL + portfolio.image} alt="icon" style={{ width: '510px', height: '310px' }} />
+					</Link>
 				</div>
 				<div className="content">
-				<h4 className="title"> 
-					<Link to={process.env.PUBLIC_URL + `/project-details/${slugify(portfolio.title)}`}>{portfolio.title}</Link>
-				</h4>
-				<span className="subtitle">
-					{portfolio.category.map((cat, i) => (
-						<span key={i}>{cat}</span>
-					))}
-				</span>
+					<h4 className="title">
+						<Link to={process.env.PUBLIC_URL + `/project-details/${slugify(portfolio.title)}`}>{portfolio.title}</Link>
+					</h4>
+					<span className="subtitle">
+						{portfolio.category.map((cat, i) => (
+							<span key={i}>{cat}</span>
+						))}
+					</span>
 				</div>
 			</div>
 		</>
-    )
+	)
 }
 
 export default PropOne;
